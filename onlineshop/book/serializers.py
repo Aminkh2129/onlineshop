8ddd3a6ecb6beea431ca8087cf420f63c6651e70
Aminkh2129 <amin.khatibi.nia.29@gmail.com>
2024-04-30 15:51:15 +0330

    add NotebookSerializers

--- onlineshop/book/serializers.py
+++ onlineshop/book/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
-#from .models import 
+from .models import Notebook,Customer,Store,Order,Order_Item
 from django.contrib.auth.models import User
+
+
+
+class NotebookSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Notebook
+        fields='__all__'
