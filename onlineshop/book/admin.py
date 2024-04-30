d3759c78f522afc5a577b570b5ef39473f4c523c
Aminkh2129 <amin.khatibi.nia.29@gmail.com>
2024-04-30 14:29:59 +0330

    edit model & input data to models & create admin.py

--- onlineshop/book/admin.py
+++ onlineshop/book/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Notebook,Customer,Store,Order,Order_Item
+
+
 
 # Register your models here.
+@admin.register(Notebook)
+class NotebookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order_Item)
+class Order_ItemAdmin(admin.ModelAdmin):
+    pass
--- onlineshop/book/models.py
+++ onlineshop/book/models.py
@@ -12,15 +12,27 @@ class Notebook(models.Model):
         ('blue','blue'),
         ('green','green'),
             ]
+    
+    PageNumber=[
+        ('40','40'),
+        ('60','60'),
+        ('80','80'),
+        ('100','100'),
+        ('200','200'),
+            ]
 
     name=models.CharField(max_length=200)
     description=models.TextField()
-    price=models.DecimalField(max_digits=5, decimal_places=2)
-    image=models.ImageField(upload_to='')
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    image=models.ImageField(upload_to='',null=True)
     publisher=models.CharField(max_length=20)
-    Telephone=models.BigIntegerField(blank=True, null=True)
+    page_number=models.CharField(blank=True, null=True,choices=PageNumber)
     color=models.CharField(max_length=20,choices=COLORS)
+    is_active=models.BooleanField(default=True)
 
+
+    def __str__(self):
+            return self.name
     class Meta:
         db_table = 'Notebook'  
 
@@ -59,6 +71,8 @@ class Order(models.Model):
     date_order=models.DateTimeField()#
     customer=models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='customer')
 
+    def __str__(self):
+            return self.id
     class Meta:
         db_table = 'Order' 
 
